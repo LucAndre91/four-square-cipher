f33449e36dc23576c24ac22f5957fe455adca6e4
Ony <ony@Apogee-Solutions-Inc-MacBook-Pro.local>
2021-12-07 14:46:21 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
@@ -19,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="container">
+      <label>Key 1: </label>
+      <input v-model="key1" @input="createSquareOne"/>
+      <br />
+      <label>Key 2: </label>
+      <input v-model="key2" @input="createSquareTwo" />
+      <br />
+      <label>Input message: </label>
+      <input v-model="inputMessage"/>
+      <br />
+      <button @click="encryptString">Encrypt</button>
+      <input v-model="encryptedMessage" />
+      <br />
+      <button @click="decryptString">Decrypt</button>
+      <input v-model="decryptedMessage"/>
+      <br />
+      <br />
+
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div v-for="row in regSquare" :key="row">
+              <span v-for="letter in row" :key="letter">
+                {{letter}} &nbsp;
+              </span>
+            </div>
+          </div>
+        
+
+
+          <div class="col">
+            <div v-for="row in squareOne" :key="row">
+              <span v-for="(letter) in row" :key="letter">
+                {{letter}} &nbsp;
+              </span>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col">
+            <div v-for="row in squareTwo" :key="row">
+              <span v-for="letter in row" :key="letter">
+                {{letter}} &nbsp;
+              </span>
+            </div>
+          </div>
+    
+          <div class=col>
+            <div v-for="row in regSquare" :key="row">
+              <span v-for="letter in row" :key="letter">
+                  {{letter}} &nbsp;
+              </span>
+            </div>
+          </div>
+    
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -35,12 +70,184 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      key1: '',
+      key2:'',
+      alphabet: 'abcdefghiklmnopqrstuvwxyz'.split(''),
+      squareOneLetters: 'abcdefghiklmnopqrstuvwxyz'.split(''),
+      squareTwoLetters: 'abcdefghiklmnopqrstuvwxyz'.split(''),
+      firstSquare: 'abcdefghiklmnopqrstuvwxyz'.split(''),
+      secondSquare: 'abcdefghiklmnopqrstuvwxyz'.split(''),
+      inputMessage: '',
+      encryptedMessage: '',
+      decryptedMessage: ''
+    }
+  },
+  methods: {
+    createSquareOne() {
+      var keyArray = this.key1.toLowerCase().replace(/j/g,'i').split('');
+      let keyWord = [];
+      let alphabet = 'abcdefghiklmnopqrstuvwxyz'.split('');
+
+      for(let i = 0; i < keyArray.length; i++) {
+        if(!keyWord.includes(keyArray[i])) {
+          keyWord.push(keyArray[i])
+        }
+      }
+
+      for(let i = 0; i < keyWord.length; i++) {
+        if(this.alphabet.includes(keyWord[i])) {
+          let index = alphabet.indexOf(keyWord[i])
+          alphabet.splice(index, 1)
+        }
+      }
+      this.squareOneLetters = keyWord.concat(alphabet);
+    },
+    createSquareTwo() {
+      var keyArray = this.key2.toLowerCase().replace(/j/g,'i').split('');
+      let keyWord = [];
+      let alphabet = 'abcdefghiklmnopqrstuvwxyz'.split('');
+      for(let i = 0; i < keyArray.length; i++) {
+        if(!keyWord.includes(keyArray[i])) {
+          keyWord.push(keyArray[i])
+        }
+      }
+      for(let i = 0; i < keyWord.length; i++) {
+        if(alphabet.includes(keyWord[i])) {
+          let index = alphabet.indexOf(keyWord[i])
+          alphabet.splice(index, 1)
+        }
+      }
+      this.squareTwoLetters = keyWord.concat(alphabet)
+    },
+    encryptString() {
+      var square1 = this.squareOneLetters;
+      var square2 = this.squareTwoLetters;
+      var alphabet = this.alphabet;
+      var messageOutput = "";
+      var messageInput = this.inputMessage.toLowerCase().replace(/j/g,'i').split('');
+      var pos = 0;
+
+      while(pos < messageInput.length) {
+        var m1 =  messageInput[pos];
+        var m2 = '';
+        
+        if(pos + 1 < messageInput.length) {
+          m2 = messageInput[pos + 1];
+          pos += 2;
+        } else {
+          m2 = 'q' //dummy letter
+          pos += 1;
+        }
+      var index1 = alphabet.indexOf(m1);
+      var index2 = alphabet.indexOf(m2);
+      var c1 = square1[(5 * Math.floor(index1 / 5)) + index2 % 5]
+      var c2 = square2[(5 * Math.floor(index2 / 5)) + index1 % 5] 
+
+       messageOutput = messageOutput.concat(c1);
+       messageOutput = messageOutput.concat(c2);
+
+      }
+        this.encryptedMessage = messageOutput
+        return messageOutput
+      
+    },
+    decryptString() {
+      var square1 = this.squareOneLetters;
+      var square2 = this.squareTwoLetters;
+      var alphabet = this.alphabet;
+      var messageOutput = "";
+      var messageInput = this.inputMessage.toLowerCase().replace(/j/g,'i').split('');
+      var pos = 0;
+
+      while(pos < messageInput.length) {
+        var m1 =  messageInput[pos];
+        var m2 = '';
+        
+        if(pos + 1 < messageInput.length) {
+          m2 = messageInput[pos + 1];
+          pos += 2;
+        } else {
+          m2 = 'q' //dummy letter
+          pos += 1;
+        }
+        var index1 = square1.indexOf(m1);
+        var index2 = square2.indexOf(m2);
+        var c1 = alphabet[(5 * Math.floor(index1 / 5)) + index2 % 5]
+        var c2 = alphabet[(5 * Math.floor(index2 / 5)) + index1 % 5]
+        messageOutput = messageOutput.concat(c1);
+        messageOutput = messageOutput.concat(c2);
+      }
+      this.decryptedMessage = messageOutput
+      return messageOutput
+    }
+  },
+  computed: {
+    squareOne() {
+      var arr = [];
+      var A = this.squareOneLetters;
+      var rows = Math.sqrt(A.length);
+      for(var i = 0; i < rows; i++) {
+        arr[i] = [];
+        for(var j = 0; j < rows; j++) {
+          arr[i][j] = A[i * rows + j];
+        }
+      }
+      
+      return arr;
+
+    },
+    squareTwo() {
+      var arr = [];
+      var A = this.squareTwoLetters;
+      var rows = Math.sqrt(A.length);
+      for(var i = 0; i < rows; i++) {
+        arr[i] = [];
+        for(var j = 0; j < rows; j++) {
+          arr[i][j] = A[i * rows + j];
+        }
+      }
+      return arr;
+    },
+    regSquare() {
+      var arr = [];
+      var A = this.alphabet;
+      var rows = Math.sqrt(A.length);
+      for(var i = 0; i < rows; i++) {
+        arr[i] = [];
+        for(var j = 0; j < rows; j++) {
+          arr[i][j] = A[i * rows + j];
+        }
+      }
+      return arr;
+    },
+
+    // makeRows() {
+    //   let squares = [this.regSquare, this.squareOne, this.regSquare, this.squareTwo];
+    //   let row = [];
+    //   let i, l, chunkSize = this.rowSize;
+
+    //   for(i = 0, l = squares.length; i < l; i += chunkSize) {
+    //     row.push(squares.slice(i, i+chunkSize))
+    //   }
+    //   return row;    
+    // }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.col {
+  border-style: solid;
+}
+.row {
+  display: flex;
+}
 h3 {
   margin: 40px 0 0;
 }
