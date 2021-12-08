1e472d0bceb71a1fbfc18c9932cbc4cd254fbd27
Ony <ony@Apogee-Solutions-Inc-MacBook-Pro.local>
2021-12-08 10:51:10 -0500

    update font, button styling

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <HelloWorld />
+    <Cipher />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cipher from './components/Cipher.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cipher
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
